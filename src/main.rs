64f5b391eb8115e33ad0ee623c503acb46dea771
T. Suzuki <st09d18@icloud.com>
2023-07-20 11:43:18 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use enigo::*;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let mut enigo = Enigo::new();
+
+    let start = time::Instant::now();
+
+    loop {
+        let elapsed = start.elapsed().as_secs_f64();
+
+        // 30秒経過したら終了
+        if elapsed > 30.0 {
+            break;
+        }
+        enigo.mouse_move_relative(1, 1);
+
+        // 1秒待機
+        thread::sleep(time::Duration::from_millis(10));
+    }
 }
